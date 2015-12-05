8c08609e4a54a23ccaaa7a285725e61b6502cd9b
knchst0704 <knchst0704@gmail.com>
2015-12-05 21:41:09 +0900

    Add Model

--- GoodWeather/Classes/Controller/ViewController.swift
+++ GoodWeather/Classes/Controller/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        APIClient.sharedInstance.getCurrentWeather(35, lon: 139)
     }
 
     override func didReceiveMemoryWarning() {
